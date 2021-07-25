f55aa05c1ed44c530f8646ca94a8dfa5827efb35
Netsl <vanya6537@gmail.com>
2021-07-26 00:10:26 +0300

    fix: add missing app files

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8a2611a33b9e5bf0233b6a9831781497'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3251342d-cae9-4ca0-9e4a-c5dd11d8daac'
+};
